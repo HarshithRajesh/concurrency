021fc3d9fb665f99e6700c31783a8a86b3058076
harshith <harshithr2004@gmail.com>
2025-06-24 21:49:31 +0530

    Conncurency race condition

---
+++ main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+)
+
+func main() {
+	var data int
+	var memoryAccess sync.Mutex
+	go func() {
+		memoryAccess.Lock()
+		data++
+		memoryAccess.Unlock()
+	}()
+	memoryAccess.Lock()
+	// time.Sleep(1 * time.Second)
+	if data == 0 {
+		fmt.Printf("the value is %v.\n", data)
+	} else {
+		fmt.Printf("the value is %v.\n", data)
+	}
+	memoryAccess.Unlock()
+}
